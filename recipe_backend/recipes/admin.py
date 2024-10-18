2df58f85955016b7075a83b31251e1e70d4fb0ea
JoseSholly <josepholawuyi30@gmail.com>
2024-10-18 14:44:27 +0100

    Update admin page

--- recipe_backend/recipes/admin.py
+++ recipe_backend/recipes/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Recipe, Ingredient
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+# Registering the Ingredient model
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id','name','get_recipes',)  # Display these fields in the admin list view
+    search_fields = ('name',)  # Add a search box for the name field
+
+    def get_recipes(self, obj):
+        # Get a comma-separated list of recipe names
+        return ', '.join([recipe.food_name for recipe in obj.recipe_set.all()])
+    get_recipes.short_description = 'Recipes'  # Set column title
+
+
+# Registering the Recipe model
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id','food_name', 'course', 'cuisine', 'servings', 'calories')  # Display these fields in the admin list view
+    search_fields = ('food_name', 'course', 'cuisine')  # Add a search box for specified fields
+    list_filter = ('course', 'cuisine')  # Add filters for course and cuisine
+    ordering = ('food_name',)  # Default ordering of the recipes
+
