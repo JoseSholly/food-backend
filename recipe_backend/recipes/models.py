3991ae84cfac0498c01db9b7471d4e54bafade9f
JoseSholly <josepholawuyi30@gmail.com>
2024-10-18 12:15:07 +0100

    Add requirements.txt

--- recipe_backend/recipes/models.py
+++ recipe_backend/recipes/models.py
@@ -5,6 +5,13 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.CharField(max_length=100)
 
+    def __str__(self):
+        # Fetch all related recipe names and join them with commas
+        recipes = self.recipe_set.all()  # Access related Recipe objects
+        recipe_names = ', '.join([recipe.food_name for recipe in recipes])
+        
+        return f"{self.name} ({recipe_names})" if recipe_names else self.name
+
 class Recipe(models.Model):
     food_name = models.CharField(max_length=255)
     preparation_time = models.CharField(max_length=100)
