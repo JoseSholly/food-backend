a25667a7f64c52646e925d54423aa831c9824297
JoseSholly <josepholawuyi30@gmail.com>
2024-11-02 17:37:49 +0100

    Serve static file

--- recipe_backend/recipe_backend/settings/prod.py
+++ recipe_backend/recipe_backend/settings/prod.py
@@ -5,12 +5,23 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = '/static/'
-
 database_url= os.getenv("DATABASE_URL")
 
 DATABASES["default"] = dj_database_url.parse(database_url)
 
 ALLOWED_HOSTS = ['127.0.0.1',]
+
+STATIC_URL = '/static/'
+
+
+# This production code might break development mode, so we check # This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    WHITENOISE_AUTOREFRESH = True
+    WHITENOISE_USE_FINDERS = True
+    WHITENOISE_COMPRESS = True
+    WHITENOISE_MANIFEST_STRICT = True 
