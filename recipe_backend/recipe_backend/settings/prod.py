0f58a4a6581d1b1a20751e68dfe57cd6d0a7067e
JoseSholly <josepholawuyi30@gmail.com>
2024-11-02 17:34:33 +0100

    Add prod database settings

--- recipe_backend/recipe_backend/settings/prod.py
+++ recipe_backend/recipe_backend/settings/prod.py
@@ -8,3 +8,9 @@ DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+database_url= os.getenv("DATABASE_URL")
+
+DATABASES["default"] = dj_database_url.parse(database_url)
+
+ALLOWED_HOSTS = ['127.0.0.1',]
