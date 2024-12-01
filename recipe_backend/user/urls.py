7036013a16bd20561b064b1213a35ddb1b2a682a
JoseSholly <josepholawuyi30@gmail.com>
2024-12-01 08:18:12 +0100

    Implement login

--- recipe_backend/user/urls.py
+++ recipe_backend/user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import CreateUserView, LoginView, LogoutView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 urlpatterns = [
-    path('user/signup/', CreateUserView.as_view(), name='signup'),
-    path('user/login/', LoginView.as_view(), name='login'),
-    path('user/logout/', LogoutView.as_view(), name='logout'),
+    # Django Auth system
+    # path('user/signup/', CreateUserView.as_view(), name='signup'),
+    # path('user/login/', LoginView.as_view(), name='login'),
+    # path('user/logout/', LogoutView.as_view(), name='logout'),
+
+    path('user/login/', TokenObtainPairView.as_view(), name='login'),
 ]
+
